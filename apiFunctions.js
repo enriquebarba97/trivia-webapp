19a45b59fe9cef71d8ab08b49d5415f3f929453a
Enrique Barba <enriquebarba97@gmail.com>
2019-12-22 18:34:10 +0100

    Add test for loading questions from the API

--- apiFunctions.js
+++ apiFunctions.js
@@ -3,19 +3,34 @@ var baseURL = "https://opentdb.com/";
 var token = null;
 
 
+document.getElementById("button").onclick = function(){
+    getQuestion(9);
+};
+
+
+
+function updateQuestion(data){
+    document.getElementById("test").innerHTML = data.results[0].question;
+}
+
+
+
+
 function sessionToken(){
     var url = baseURL + "api_token.php?command=request";
     var req = new XMLHttpRequest()
     req.onreadystatechange = function(){
-        if(req.readyState==4 && req.status==200){
-            var response = JSON.parse(req.responseText);
-            if(response.response_code == 0){
-                token = response.token;
+        if(req.readyState==4){
+            if(req.status==200){
+                var response = JSON.parse(req.responseText);
+                if(response.response_code == 0){
+                    token = response.token;
+                }else{
+                    alert("Server error: " + response.response_message);
+                }
             }else{
                 alert("Server error: " + response.response_message);
             }
-        }else{
-            alert("There was a problem contacting with the server: "+ req.statusText);
         }
     }
     req.open("get", url, true);
@@ -24,8 +39,21 @@ function sessionToken(){
 
 function getQuestion(category){
     var url = baseURL + "api.php?amount=1&category="+category;
-    var req = new XMLHttpRequest()
-    req.onreadystatechange = updateQuestion()
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function(){
+        if(req.readyState==4){
+            if(req.status==200){
+                var response = JSON.parse(req.responseText);
+                if(response.response_code == 0){
+                    updateQuestion(JSON.parse(req.responseText));
+                }else{
+                    alert("Server error: " + response.response_message);
+                }
+            }else{
+                alert("Server error: " + response.response_message);
+            }
+        }
+    }
     req.open("get", url, true);
     req.send(null);
 }
