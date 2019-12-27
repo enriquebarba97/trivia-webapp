b13f76f3c084a075f7e2ae3a8f9cd9d60fe31641
Enrique Barba <enriquebarba97@gmail.com>
2019-12-27 13:43:45 +0100

    Add category IDs

--- apiFunctions.js
+++ apiFunctions.js
@@ -29,6 +29,32 @@ function sessionToken(){
     req.send(null);
 }
 
+
+// "trivia_categories":
+//       9,  "General Knowledge"
+//       10, "Entertainment: Books"
+//       11, "Entertainment: Film"
+//       12, "Entertainment: Music"
+//       13, "Entertainment: Musicals & Theatres"
+//       14, "Entertainment: Television"
+//       15, "Entertainment: Video Games"
+//       16, "Entertainment: Board Games"
+//       17, "Science & Nature"
+//       18, "Science: Computers"
+//       19, "Science: Mathematics"
+//       20, "Mythology"
+//       21, "Sports"
+//       22, "Geography"
+//       23, "History"
+//       24, "Politics"
+//       25, "Art"
+//       26, "Celebrities"
+//       27, "Animals"
+//       28, "Vehicles"
+//       29, "Entertainment: Comics"
+//       30, "Science: Gadgets"
+//       31, "Entertainment: Japanese Anime & Manga"
+//       32, "Entertainment: Cartoon & Animations"
 function getQuestion(category){
     if(token==null){sessionToken();}
     var url = baseURL + "api.php?amount=1&type=multiple&category="+category;
