5800d6df0a62812d111ebd6daa786e3637ff2068
Christopher Oser <christopher.oser@outlook.com>
2019-12-28 18:15:29 +0100

    added the dice and some css

--- src/controller.js
+++ src/controller.js
@@ -3,7 +3,15 @@ class controller{
 		this.model = new model();
 		this.view = new view();
 	}
+
 	run(){
-		this.view.draw();
+        var _this = this;
+		_this.view.draw();
+        document.getElementById("dice_number").innerHTML = "0";
+
+        document.getElementById("roll").onclick = function(){
+            document.getElementById("dice_number").innerHTML = "...";
+            window.setTimeout(function() {_this.model.setNewDiceNumber(document.getElementById("dice_number"))}, 500);
+        };
 	}
 }
--- src/model.js
+++ src/model.js
@@ -1,5 +1,18 @@
 class model{
 	constructor(){
+        this.dice = {
+            sides: 6,
+            roll: function () {
+                var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+                return randomNumber;
+            }
+        }
 
+        this.dice_value = 0;
 	}
+    setNewDiceNumber(field)
+    {
+        this.dice_value = this.dice.roll();
+        field.innerHTML = this.dice_value;
+    }
 }
