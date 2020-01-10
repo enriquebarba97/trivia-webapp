ccb9c51dbda6c1483a7b54de796801e2781cd8e4
Enrique Barba <enriquebarba97@gmail.com>
2020-01-10 18:41:52 +0100

    Changed to standard Websockets API Added sync movement

---
+++ online.js
@@ -0,0 +1,378 @@
+const socket = new WebSocket("wss://connect.websocket.in/v2/14?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjVlMTc3Yjk0NzgwNzQ3NmRiOWYyNTQwYzI3M2I1MGVkZTIzN2RiMzNiOGVhODI3NjE3MjA0ZWZkNDRmZGE0M2MwZmU4MDcwMjhlNThmM2VkIn0.eyJhdWQiOiI4IiwianRpIjoiNWUxNzdiOTQ3ODA3NDc2ZGI5ZjI1NDBjMjczYjUwZWRlMjM3ZGIzM2I4ZWE4Mjc2MTcyMDRlZmQ0NGZkYTQzYzBmZTgwNzAyOGU1OGYzZWQiLCJpYXQiOjE1NzgyMjkyMjEsIm5iZiI6MTU3ODIyOTIyMSwiZXhwIjoxNjA5ODUxNjIxLCJzdWIiOiIyNDUiLCJzY29wZXMiOltdfQ.fXiYOMmb8mG-Zse8jQTYV_M8QOZm0o3MZ8NNTIlWrgMqbq4ZrzNpuCcsqxuRrGMTfvLhH_G3DoNCVI6JbyaYEKhjGZgXwd3sGJ_43Fjuh9lCWYKd5HDPcrPFK4LmTzM_qCZtBCh3t6lSREOuisnswdnABw-b9kGxGOPFzpZlDmRGa3xUoHyU6Fm-uQ6A6Se0TJKBrYngVYzQpceiNkLczEyUUiyzYfEjH5k9PDQ6BO3dlW6lWbl73plcS8KXN4KYxTUctq6gEJkOhVrL3vcnml4nleFwh4BTwCbFxNtxDbNOzfs4je28I-yoyTdDLcbO2YYl72ygZWKRQUYoeRYYAkbXrw74n4wlIbF2su2qOBSeb9tC_g41VU4Eex8fLUxs3Dp5gO7CF-Xpp5t_GEU7grjcm-t_Ctjz6uZUE93KGuMdYXeVffJh9lfb66GSffZ5wNS6SqG7FowRiTS0J7Isqhvspi8m-lu4vNio_ueCV_nLY5obIPDFWtSfNEDvKFO94E-IkNswvImBXOH4zNFjW3jxlbh8Zz-mMco6W8TkAhgQ_C8XHotn6ndHqp5461d4BOi9T0Z3KQWDm5PTaEpw1qHlKcAxBb2sS12ngiZeusQKt0TaCFQR8SzHioTwVAO0aNQhZ3PItG4NparPRg0cglsCPFfz6wXIudCkp53fPzc");
+
+
+//DOM elements
+
+let startgame = document.getElementById("start");
+let player1 = document.getElementById("active1");
+let player2 = document.getElementById("active2");
+let player3 = document.getElementById("active3");
+let player4 = document.getElementById("active4");
+let name1 = document.getElementById("name1");
+let name2 = document.getElementById("name2");
+let name3 = document.getElementById("name3");
+let name4 = document.getElementById("name4");
+let roll = document.getElementById("roll");
+let dice_number = document.getElementById("dice_number");
+let myCanvas = document.getElementById("myCanvas");
+let new_question_button = document.getElementById("new_question_button");
+let text_question = document.getElementById("text_question");
+let category1 = document.getElementById("category1");
+let category2 = document.getElementById("category2");
+let category3 = document.getElementById("category3");
+let category4 = document.getElementById("category4");
+let check_answer_button = document.getElementById("check_answer_button");
+let cpu1 = document.getElementById("cpu1");
+let cpu2 = document.getElementById("cpu2");
+let cpu3 = document.getElementById("cpu3");
+let cpu4 = document.getElementById("cpu4");
+let human1 = document.getElementById("human1");
+let human2 = document.getElementById("human2");
+let human3 = document.getElementById("human3");
+let human4 = document.getElementById("human4");
+let answer_0 = document.getElementById("answer_0");
+let answer_1 = document.getElementById("answer_1");
+let answer_2 = document.getElementById("answer_2");
+let answer_3 = document.getElementById("answer_3");
+
+
+
+
+// Sending to the server notifications
+
+
+player1.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Player added";
+  data.player = 1;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+player2.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Player added";
+  data.player = 2;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+player3.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Player added";
+  data.player = 3;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+player4.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Player added";
+  data.player = 4;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+name1.addEventListener("keyup", function(){
+  var mes = {};
+  mes.event = "Name changed";
+  var data = {};
+  data.player = 1;
+  data.name = this.value;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+name2.addEventListener("keyup", function(){
+  var mes = {};
+  mes.event = "Name changed";
+  var data = {};
+  data.player = 2;
+  data.name = this.value;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+name3.addEventListener("keyup", function(){
+  var mes = {};
+  mes.event = "Name changed";
+  var data = {};
+  data.player = 3;
+  data.name = this.value;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+name4.addEventListener("keyup", function(){
+  var mes = {};
+  mes.event = "Name changed";
+  var data = {};
+  data.player = 4;
+  data.name = this.value;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+
+startgame.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Starting";
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+roll.addEventListener("click", function() {
+setTimeout(function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Rolling";
+  data.roll = dice_number.innerHTML;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+},600);
+});
+
+// myCanvas.addEventListener("click", function() {     //Not working yet
+// setTimeout(function() {  
+//   socket.emit("Moving Players", myCanvas);
+// },6);
+// });
+
+category1.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Selected Category";
+  data.cat = 1;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+category2.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Selected Category";
+  data.cat = 2;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+category3.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Selected Category";
+  data.cat = 3;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+category4.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Selected Category";
+  data.cat = 4;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+
+// new_question_button.addEventListener("click", function() {
+//   window.setTimeout(function(){
+//   socket.emit("New question", text_question.innerHTML);
+//   }, 100)
+// });
+
+check_answer_button.addEventListener("click", function() {
+  var mes = {};
+  mes.event = "Checking answer";
+  socket.send(JSON.stringify(mes));
+});
+
+cpu1.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Changing cpu";
+  data.cpu = 1;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+cpu2.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Changing cpu";
+  data.cpu = 2;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+cpu3.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Changing cpu";
+  data.cpu = 3;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+cpu4.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Changing cpu";
+  data.cpu = 4;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+human1.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Changing human";
+  data.human = 1;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+human2.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Changing human";
+  data.cpu = 2;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+human3.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Changing human";
+  data.cpu = 3;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+human4.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Changing human";
+  data.cpu = 4;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+answer_0.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Answer selected";
+  data.ans = 0;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+});
+
+answer_1.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Answer selected";
+  data.ans = 1;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+ });
+
+ answer_2.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Answer selected";
+  data.ans = 2;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+ });
+
+ answer_3.addEventListener("click", function() {
+  var mes = {};
+  var data = {};
+  mes.event = "Answer selected";
+  data.ans = 3;
+  mes.data = data;
+  socket.send(JSON.stringify(mes));
+ });
+
+// // Listening to the server and changing in all clients
+
+
+socket.onmessage = function (e){
+  console.log(e);
+  var mes = JSON.parse(e.data);
+  var data = mes.data;
+  
+  switch(mes.event){
+    case "Player added":
+      controller.instance().set_player_config_active(data.player);
+      break;
+    case "Name changed":
+      var data = mes.data;
+      document.getElementById("name"+data.player).value = data.name;
+      break;
+    case "Starting":
+      controller.instance().start();
+      break;
+    case "Rolling":
+      dice_number.innerHTML = data.roll;
+      break;
+    case "Selected Category":
+      document.getElementById("category"+data.cat).checked = true;
+      break;
+    case "New question":
+      var result = data.question;
+      var num = result.cpos;
+      var j = 0;
+      var letter = 'a';
+
+      document.getElementById("question").innerHTML = result.question;
+		
+		  for(var i = 0; i< 4; i++){
+			  if(i==num){
+				  var answer = letter + ": " + result.correct_answer;
+				  document.getElementById("label_"+i).innerHTML = answer;
+				  document.getElementById("answer_"+i).value = "correct";
+			  }else{
+				  var answer = letter + ": " + result.incorrect_answers[j];
+				  document.getElementById("label_"+i).innerHTML = answer;
+				  document.getElementById("answer_"+i).value = "incorrect";
+				  j++;
+			  }
+			  letter = String.fromCharCode(letter.charCodeAt(0) + 1);
+      }
+      break;
+    case "Checking answer":
+      controller.instance().checkAnswerButtonClicked();
+      break;
+    case "Answer selected":
+      document.getElementById("answer_"+data.ans).checked = true;
+      break;
+    case "Changing cpu":
+      document.getElementById("cpu"+data.cpu).checked = true;
+      break;
+    case "Changing human":
+      document.getElementById("human"+data.human).checked = true;
+      break;
+    case "Moving player":
+      var cp = model.instance().current_player;
+      cp.x = data.x;
+      cp.y = data.y;
+      break
+  }
+};
+
+
+// socket.on("Moving Players", function(data) {
+//   console.log("moving");
+//   //myCanvas= data;
+//   //view.instance();
+// });
+
+
+
---
+++ src/controller.js
@@ -0,0 +1,318 @@
+class controller{
+	instance_ = null;
+	constructor(){
+        document.getElementById("dice_number").innerHTML = "0";
+
+        model.instance().player1_active = false;
+        model.instance().player2_active = false;
+        model.instance().player3_active = false;
+        model.instance().player4_active = false;
+
+        document.getElementById("roll").onclick = function(){
+        	controller.instance().roll_die();
+        };
+
+        document.getElementById("active1").onclick = function(){
+           controller.instance().set_player_config_active(1);
+        };
+
+        document.getElementById("active2").onclick = function(){
+            controller.instance().set_player_config_active(2);
+        };
+
+        document.getElementById("active3").onclick = function(){
+            controller.instance().set_player_config_active(3);
+        };
+
+        document.getElementById("active4").onclick = function(){
+            controller.instance().set_player_config_active(4);
+        };
+
+        document.getElementById("new_question_button").onclick = function(){
+            var question = controller.instance().newQuestionButtonClicked();
+            var mes = {};
+            var data = {};
+            mes.event = "New question";
+            data.question = question;
+            mes.data = data;
+            socket.send(JSON.stringify(mes));
+                // window.setTimeout(function(){
+                // socket.emit("New question", question);
+                // }, 100)
+		};
+
+		document.getElementById("check_answer_button").onclick = function(){
+            controller.instance().checkAnswerButtonClicked();
+		};
+
+        document.getElementById("start").onclick = function(){
+        	controller.instance().start();
+        };
+
+        document.getElementById("doc").onclick = function() {
+            controller.instance().showDoc();
+        }
+
+        this.question_history_counter = 1;
+	}
+
+    showDoc()
+    {
+        alert(documentation);
+    }
+
+    newQuestionButtonClicked()
+    {
+        var c = controller.instance();
+        var category_buttons = document.getElementsByName("category");
+        c.desired_category = 0;
+        for(var cat of category_buttons)
+        {
+            if (cat.checked) 
+            {
+                c.desired_category = parseInt(cat.value);
+            }
+        }
+        if (c.desired_category == 0)
+        {
+            alert("No Category Selected!");
+            return;
+        }
+        var question = model.instance().getQuestion(c.desired_category);
+        if(model.instance().current_player.cpu){
+            console.log("CPU Turn")
+            window.setTimeout(controller.instance().cpuPick, 2000);
+        }
+        return question;
+    }
+
+    checkAnswerButtonClicked()
+    {
+        var c = controller.instance();
+        var check = c.sanityCheck();
+        if (check == false) {return;}
+
+        var correct = model.instance().checkAnswer();
+        if (correct){view.instance().answerCorrect();}
+        else{view.instance().answerWrong();}
+
+
+        var new_question    = document.createElement("div");
+
+        if (correct) {new_question.className = "qh_question_true";}
+        else{new_question.className = "qh_question_false";}
+        
+        var question_text = document.createTextNode((controller.instance().question_history_counter).toString() + ".) " + document.getElementById("question").innerHTML);
+        controller.instance().question_history_counter++;
+        new_question.appendChild(question_text);
+
+        document.getElementById("q_history").insertBefore(new_question, document.getElementById("q_history").firstChild);
+
+
+        view.instance().uncheckCategories();
+        view.instance().clearQuestion();
+        
+        var win = c.checkScore()
+        if (win) {
+            alert("Player " + model.instance().current_player.number + ": " + model.instance().current_player.name + " has won! Congratulations!");
+            window.setTimeout(view.instance().reset, 1000);
+            return;
+        }
+
+        controller.instance().nextPlayer();
+        if(model.instance().current_player.cpu){
+            alert("Player " + model.instance().current_player.number + ": " + model.instance().current_player.name + " is rolling the dice. Please move the player's token accordingly and fetch a question.");
+            controller.instance().roll_die();
+        }
+    }
+
+    checkScore()
+    {
+        for(var value of model.instance().current_player.categories_correct) {
+            if(value == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    sanityCheck()
+    {
+        var answers = document.getElementsByName("answer");
+        if (answers.length == 0) {
+            //alert("No Question Fetched!");
+            return false;
+        }
+        return true;
+    }
+
+
+	roll_die(){
+        document.getElementById("dice_number").innerHTML = "...";
+        window.setTimeout(function() {model.instance().setNewDiceNumber(document.getElementById("dice_number"))}, 500);
+    }
+
+
+    set_player_config_active(number)
+    {
+        if (document.getElementById("player_config" + number.toString()).style.backgroundColor == "green") 
+        {
+            switch(number) {
+                case 1:
+                  model.instance().player1_active = false;
+                  break;
+                case 2:
+                  model.instance().player2_active = false;
+                  break;
+                case 3:
+                  model.instance().player3_active = false;
+                  break;
+                case 4:
+                  model.instance().player4_active = false;
+                  break;
+                default:
+            } 
+            document.getElementById("player_config" + number.toString()).style.backgroundColor = "#4d4d4d";
+            document.getElementById("player_config" + number.toString()).style.border = "1px solid #4d4d4d";
+        }
+        else
+        {
+            switch(number) {
+                case 1:
+                  model.instance().player1_active = true;
+                  break;
+                case 2:
+                  model.instance().player2_active = true;
+                  break;
+                case 3:
+                  model.instance().player3_active = true;
+                  break;
+                case 4:
+                  model.instance().player4_active = true;
+                  break;
+                default:
+            }
+            document.getElementById("player_config" + number.toString()).style.backgroundColor = "green"
+            document.getElementById("player_config" + number.toString()).style.border = "1px solid #000000";
+        }
+    }
+
+    start()
+    {
+        var m = model.instance();
+        var activations = [m.player1_active, m.player2_active, m.player3_active, m.player4_active];
+        var counter = 1;
+
+        var number_of_players = 0;
+        for(var act of activations){
+        	if(act){
+        		number_of_players++;
+        	}
+        }
+
+        if(number_of_players == 0){return;}
+
+        for (var i = 0; i < activations.length; i++) {
+            if (activations[i] == true)
+            {
+                var name = document.getElementById("name" + (i+1).toString()).value;
+                var cpu = false;
+                var radioButtons = document.getElementsByName("config" + (i+1).toString());
+                if(radioButtons[0].checked){
+                	cpu = true;
+                }
+
+                model.instance().players.push(new player(counter, name, cpu, view.instance().player_colors[i], number_of_players))
+
+                counter = counter + 1;
+            };
+        }
+
+        m.current_player = m.players[0];
+        document.getElementById("current_player").innerHTML = "Player " + m.current_player.number.toString() + ": " + m.current_player.name;
+        document.getElementById("color_indicator").style.backgroundColor = m.current_player.color;
+
+        document.getElementById("game_config").style.display = "none";
+
+        for(var play of m.players)
+        {
+            var newPlayer = document.createElement("div");
+            var red       = document.createElement("div");
+            var yellow    = document.createElement("div");
+            var blue      = document.createElement("div");
+            var green     = document.createElement("div");
+            var name      = document.createElement("div");
+
+            newPlayer.className = "player_score";
+            name.className      = "score_name";
+
+            red.className       = "red";
+            yellow.className    = "yellow";
+            blue.className      = "blue";
+            green.className     = "green";
+
+            red.id          = "red_" + play.number.toString();
+            yellow.id       = "yellow_" + play.number.toString();
+            blue.id         = "blue_" + play.number.toString();
+            green.id        = "green_" + play.number.toString();
+
+            var text_name = document.createTextNode("Player " + play.number.toString() + ": " + play.name);
+            name.appendChild(text_name);
+
+            newPlayer.appendChild(name);
+            newPlayer.appendChild(red);
+            newPlayer.appendChild(yellow);
+            newPlayer.appendChild(blue);
+            newPlayer.appendChild(green);
+
+            document.getElementById("score_sheet").appendChild(newPlayer);
+        }
+
+        view.instance().turnGameModeOn();
+
+
+        if (m.current_player.cpu){
+            alert("Player " + m.current_player.number + ": " + m.current_player.name + " is rolling the dice. Please move the player's token accordingly and fetch a question.");
+            controller.instance().roll_die();
+        }
+    }
+
+
+	nextPlayer(){
+		var m = model.instance();
+		for(var i = 0; i < m.players.length; ++i){
+			if(m.players[i] == m.current_player){
+				if(i == m.players.length - 1){
+					m.current_player = m.players[0];
+					break;
+				}
+				else{
+					m.current_player = m.players[i + 1];
+					break;
+				}
+			}
+		}
+		document.getElementById("current_player").innerHTML = "Player " + m.current_player.number.toString() + ": " + m.current_player.name;
+        document.getElementById("color_indicator").style.backgroundColor = m.current_player.color;
+	}
+
+
+	cpuPick(){
+		var random = Math.floor(Math.random() * 4);
+		var answers = document.getElementsByName("answer");
+        answers[random].checked = true;
+        view.instance().highlightAnswer(random);
+        window.setTimeout(function() {view.instance().lowlightAnswer(random)}, 1000);
+	}
+
+	static instance(){
+		if(controller.instance_ == null){
+			controller.instance_ = new controller();
+			return controller.instance_;
+		}
+		else{
+			return controller.instance_;
+		}
+	}
+}
